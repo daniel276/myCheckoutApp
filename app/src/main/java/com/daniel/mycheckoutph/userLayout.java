c1f84494531387ee54095340f4c74cad2e8168cd
Daniel Juanda <danieljuanda276@gmail.com>
2017-01-09 18:22:46 +0700

    idk

---
+++ app/src/main/java/com/daniel/mycheckoutph/userLayout.java
@@ -0,0 +1,20 @@
+package com.daniel.mycheckoutph;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Daniel on 1/9/2017.
+ */
+
+public class userLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_layout);
+    }
+
+
+
+}
