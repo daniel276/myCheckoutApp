a47c1ba1571e15d6c57bdafa314c4495470c1a44
Daniel Juanda <danieljuanda276@gmail.com>
2017-01-10 11:25:43 +0700

    add all

---
+++ app/src/main/java/com/daniel/mycheckoutph/loginPage.java
@@ -0,0 +1,44 @@
+package com.daniel.mycheckoutph;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class loginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+    public void loginClick(View logClick){
+        EditText username = (EditText) findViewById(R.id.usernameTextEdit);
+        EditText password = (EditText) findViewById(R.id.passwordTextEdit);
+
+        boolean loginValid = isValid(username.getText().toString(),password.getText().toString());
+        if(loginValid){
+            Intent userPage = new Intent(this, userLayout.class);
+            startActivity(userPage);
+        }
+        else{
+            Toast.makeText(this, "Incorrect Username/Password", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isValid(String username,String password){
+        if(username.equals("admin") && password.equals("passwurd"))
+            return true;
+        else
+            return false;
+    }
+
+    public void signUpClick(View signup){
+        Intent clickSignup = new Intent(this, signUp.class);
+        startActivity(clickSignup);
+    }
+
+}
---
+++ app/src/main/java/com/daniel/mycheckoutph/signUp.java
@@ -0,0 +1,16 @@
+package com.daniel.mycheckoutph;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Daniel on 1/9/2017.
+ */
+
+public class signUp extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
